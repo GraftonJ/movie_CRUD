d0288e450a4985828f9a6ab0d17ad14326b1d76e
Josh <grafton.josh@gmail.com>
2018-10-16 21:07:44 -0600

    Migration table works

---
+++ migrations/20181016205546_all_movies.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('all_movies', function(table) {
+   // TABLE COLUMN DEFINITIONS HERE
+   table.increments()   // id field, auto PK
+   table.string('title').notNullable()
+   table.string('director').notNullable()
+   table.integer('year')
+   table.integer('rating')
+   table.string('poster_link')
+ })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('all_movies')
+}
