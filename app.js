396a034b0a8d5ca329a929532b85c5c8c6b05edc
Josh <grafton.josh@gmail.com>
2018-10-19 09:37:56 -0600

    Added index route back

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+ var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 var all_moviesRouter = require('./routes/all_movies');
 
 var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 app.use('/all_movies', all_moviesRouter);
 
 // catch 404 and forward to error handler
